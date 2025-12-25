480df64a4c7e746712f9c088006c3f213b979b2c
johanneskhage <johannesk.hage@gmail.com>
2025-12-25 20:03:33 +0100

    complete PunishmentBox

--- components/EventDetails/RegistrationCard/PunishmentBox.tsx
+++ components/EventDetails/RegistrationCard/PunishmentBox.tsx
@@ -1,10 +1,48 @@
 import React from "react"
-import type { Punishment } from "../../types/event"
+import { View, Text, TouchableOpacity, StyleSheet, Linking } from "react-native"
+import type { Punishment } from "../../../types/punishment"
+import { Ionicons, FontAwesome6, MaterialCommunityIcons, Feather } from "@expo/vector-icons";
+import { useTheme, withAlpha, darken } from "utils/theme";
 
 interface Props {
   punishment: Punishment
 }
 
-export const PunishmentBox: React.FC<Props> = () => null
+export const PunishmentBox: React.FC<Props> =  ({ punishment }) => {
+  const theme = useTheme();
+
+  if (punishment.suspended) {
+    return (
+      <View >
+        <View >
+          <Feather name="alert-triangle"/>
+          <Text>Du er suspendert</Text>
+        </View>
+
+        <Text>
+          Gå til profilen din på OW for å se detaljer.
+        </Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={{backgroundColor: theme.errorContainer, padding: 15, borderRadius: 12, gap: 5}}>
+      <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+        <Feather name="alert-triangle" color={theme.onErrorContainer} size={17}/>
+        <Text style={{ fontSize: 16, color: theme.onErrorContainer }}>{punishment.delay} timer utsatt påmelding pga. prikk</Text>
+      </View>
+
+      <Text style={{ color: theme.onErrorContainer }}>
+        Du <Text style={{ fontWeight: 'bold' }}>kan fortsatt melde deg på</Text> ved påmeldingsstart,{'\n'}
+        men du vil være i venteliste til utsettelsen er over.
+      </Text>
+
+      <Text style={{ color: theme.onErrorContainer }}>
+          Gå til profilen din på OW for å se detaljer.
+      </Text>
+    </View>
+  )
+}
 
 export default PunishmentBox
