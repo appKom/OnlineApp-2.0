ece2eae9d5f581f8c626d9d4ab94484747fc3e1f
Erlend Strøm <erl1ls@outlook.com>
2026-02-01 15:04:22 +0100

    Change padding behaviour for scroll view

--- components/TabScreenContainer.tsx
+++ components/TabScreenContainer.tsx
@@ -1,12 +1,17 @@
 import { View } from "react-native";
-import { useTheme } from "../utils/theme"
+import { useTheme } from "../utils/theme";
 
-export function TabScreenContainer({ children }: { children: React.ReactNode }) {
+export function TabScreenContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const theme = useTheme();
 
   return (
-    <View style={{ flex: 1, paddingBottom: 104, backgroundColor: theme.background }}>
+    <View style={{ flex: 1, backgroundColor: theme.background }}>
       {children}
+      <View style={{ height: 104 }}></View>
     </View>
   );
 }
